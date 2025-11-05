3f3e2fb8944fd49c16306110c10746176227a456
rakshitbagait <rbagait2018@gmail.com>
2025-11-05 22:09:31 +0530

    app

--- app.py
+++ app.py
@@ -1,7 +1,26 @@
 from flask import Flask , render_template, request
+from flask_mail import Mail,Message
+import os 
+from dotenv import load_dotenv
+import mysql.connector
+import markdown
+from smtplib import SMTP
 
-app = Flask()
+app = Flask(__name__)
+def connection_db():
+    bakery_db = mysql.connector.connect(
+        host = "localhost",
+        user= "root",
+        password = "password",
+        database = "Bakery_db",
+        use_pure = True,
+        port = 3306
+    )
 
+    return bakery_db
+@app.route("/")
+def login():
+    return render_template("login.html")
 if __name__ == "__main__":
     app.run(debug = True)
     
