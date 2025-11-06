ed011dd6625e291e2a5fd5264324d6c40c321d24
rakshitbagait <rbagait2018@gmail.com>
2025-11-06 23:44:05 +0530

    app

--- app.py
+++ app.py
@@ -21,6 +21,10 @@ def connection_db():
 @app.route("/")
 def login():
     return render_template("login.html")
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
+
 if __name__ == "__main__":
     app.run(debug = True)
     
